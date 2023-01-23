019082ccc79acbcebd25820fb907f5f677dc8262
Mohammad Hadiomid <hadiomidm@gmail.com>
2023-01-23 11:23:28 +0000

    Define last sheet as default sheet / Update report function to read data from spreadsheet

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
 
 
+last_event = spreadsheet.worksheets()[-1].id
+print(last_event)
+event = spreadsheet.get_worksheet_by_id(last_event)
+
+
 @bot.message_handler(commands=['friends'])
 def names(message):
   names = message.text.split('\n')[1:]
   template_id = spreadsheet.worksheet("Template").id
   new_sheet_i = len(spreadsheet._spreadsheets_get()['sheets'])
+  # new_sheet_i = len(spreadsheet.worksheets())
+  global event
   event = spreadsheet.duplicate_sheet(template_id, insert_sheet_index=new_sheet_i, new_sheet_name=names[0])
   i = 2
   for name in names[1:]:
     event.update_cell(i, 1, name)
+    event.update_cell(i, 2, 0)
     i += 1
-  bot.reply_to(message, 'دورهمي جديد و اسامي دوستان ثبت شد')
+  bot.reply_to(message, f'دورهمي ({names[0]}) و اسامي دوستان ثبت شد')
 
 
 @bot.message_handler(commands=['pay'])
@@ -26,18 +34,19 @@ def payment(message):
   user = message.text.split('\n')[2]
   payment = message.text.split('\n')[3]
   bot.send_message(message.chat.id, f'{user} paid {payment}$ for {event}')
+  # bot.send_message(message.chat.id, f' مبلغ {payment} تومان توسط {user} بابت دورهمی {event} پرداخت شد')
 
 
 @bot.message_handler(commands=['report'])
 def get_report(message):
+  title = f"{event.title}\n\n"
+  col = f"{'name' : <20}{'payment' : ^10}\n"
   response = ""
-  title = 'Hangout'
-  title = f"{title}\n\n"
-  col = f"{'name' : <15}{'price' : ^11}{'paid' : >11}\n"
-  name = 'Sara'
-  price = '140'
-  paid = True
-  response = f"{name : <20}{price : ^12}{paid : >14}\n"
+  i = 2
+  while event.cell(i,1).value != None:
+    response += f"{event.cell(i,1).value : <20}{event.cell(i,2).value : ^10}\n"
+    print(response)
+    i += 1
   data = title+col+response
   bot.send_message(message.chat.id, data)
 
