407e0ef9de47fe2b6f96ab75f1ee5b015a983508
Mohammad Hadiomid <hadiomidm@gmail.com>
2023-01-23 11:44:03 +0000

    Code refactoring

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ from Gsheet import spreadsheet
 API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
 
-
-last_event = spreadsheet.worksheets()[-1].id
-print(last_event)
-event = spreadsheet.get_worksheet_by_id(last_event)
+event = spreadsheet.worksheets()[-1]
 
 
 @bot.message_handler(commands=['friends'])
 def names(message):
   names = message.text.split('\n')[1:]
   template_id = spreadsheet.worksheet("Template").id
-  new_sheet_i = len(spreadsheet._spreadsheets_get()['sheets'])
-  # new_sheet_i = len(spreadsheet.worksheets())
+  new_sheet_i = len(spreadsheet.worksheets())
   global event
   event = spreadsheet.duplicate_sheet(template_id, insert_sheet_index=new_sheet_i, new_sheet_name=names[0])
   i = 2
@@ -45,9 +41,8 @@ def get_report(message):
   i = 2
   while event.cell(i,1).value != None:
     response += f"{event.cell(i,1).value : <20}{event.cell(i,2).value : ^10}\n"
-    print(response)
     i += 1
-  data = title+col+response
+  data = title + col + response
   bot.send_message(message.chat.id, data)
 
 
